c6a336c33614fdd0b3fc3180480ae0ab1d29889a
Brian Sahota <64430701+scratchytube@users.noreply.github.com>
2025-04-30 11:49:58 -0400

    added post request function and retooled load balancer handler

--- load-balancer/load_balancer.py
+++ load-balancer/load_balancer.py
@@ -20,16 +20,29 @@ current_server = 0
 class LoadBalancerHandler(BaseHTTPRequestHandler):
     # runs every time we receive a get request
     def do_GET(self):
+        self.forward_request(method = "GET")
+    # runs every time we get a post request
+    def do_POST(self):
+        self.forward_request(method = "POST")
+
+    def forward_request(self, method):
         # modifies a variable outside our class
         global current_server
-
-        # cycles through our backend servers in order using modulo math
+        # cycles through our backend servers in order using modulo math aka round-robin logic
         backend_url = backend_servers[current_server]
         current_server = (current_server + 1) % len(backend_servers)
 
         try:
-            # sends a get request to selected backend
-            response = requests.get(backend_url)
+            if method == "GET":
+                # sends a get request to selected backend
+                response = requests.get(backend_url)
+            elif method == "POST":
+                # figures out how many bytes of data are in the request body
+                content_length = int(self.headers.get("Content-Length", 0))
+                #actually reads the body data
+                post_data = self.rfile.read(content_length)
+                # forwards the data to the backend with the content type as json
+                response = requests.post(backend_url, data = post_data, headers={"Content-Type": self.headers["Content-Type"]})
 
             # sends back a http code (200, 500)
             self.send_response(response.status_code)
@@ -37,7 +50,6 @@ class LoadBalancerHandler(BaseHTTPRequestHandler):
             self.send_header("Content-type", "application/json")
             # tells browser this is the end of the http response headers
             self.end_headers()
-
             # writes the json from the backend to the browser
             self.wfile.write(response.content)
 
